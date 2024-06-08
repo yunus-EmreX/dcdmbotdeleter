5fc15173199385d27b03280a427ef39b05413f6d
yunus-EmreX <138483345+yunus-EmreX@users.noreply.github.com>
2024-06-09 01:31:37 +0300

    Update deletedmbot.py

--- deletedmbot.py
+++ deletedmbot.py
@@ -20,4 +20,4 @@ async def purge(ctx, count: int):
     await ctx.send(f"{len(deleted)} mesaj silindi.", delete_after=5)  # Silinen mesaj sayısını gönder ve 5 saniye sonra sil
 
 # Discord botunuzun token'ını buraya yapıştırın
-bot.run("MTEyMzIxODU0MzYxMDE4NzgyNg.G-4gfS.wyVHNmbxkEuvuZtBYtM6Iii_nZwpmnTx3zNEnc")
+bot.run("")
